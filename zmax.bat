4e1a6f22bef869b5df1ac189021100d7c405ca00
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2022-02-26 07:19:01 +0600

    1st commit

--- zmax.bat
+++ zmax.bat
@@ -1,4 +1,4 @@
 git pull
 git add .
 git commit -m "1st commit"
-git push https://nishan-paul-2022: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@github.com/nishan-paul-2022/recrutar.git --all
+git push https://nishan-paul-2022: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@github.com/nishan-paul-2022/recrutar.git --all
