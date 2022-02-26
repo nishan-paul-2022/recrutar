7c5fec3e2786db3493aabe2adcf387db7a2513b0
nishan-paul-2022 <nishan.paul.hello@gmail.com>
2022-02-26 17:11:53 +0600

    1st commit

--- git_update.bat
+++ git_update.bat
@@ -1,4 +1,4 @@
 git pull
 git add .
 git commit -m "1st commit"
-git push https://nishan-paul-2022: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@github.com/nishan-paul-2022/recrutar.git --all
+git push https://nishan-paul-2022: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@github.com/nishan-paul-2022/recrutar.git --all
